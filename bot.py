6ec2ccacb76182839784ca9e5e4ab23582b0c958
alena-yudzina <lenok.ok.lol@gmail.com>
2021-08-21 19:35:24 +0300

    Move starting bot and uploading images to upload_images()

--- bot.py
+++ bot.py
@@ -7,6 +7,19 @@ from fetch_spacex import fetch_spacex_last_launch
 from dotenv import load_dotenv
 
 
+def upload_images(images_folder):
+    bot = telegram.Bot(token=os.environ['BOT_TOKEN'])
+    photos = os.listdir(images_folder)
+    while True:
+        for photo in photos:
+            with open('{0}/{1}'.format(images_folder, photo), 'rb') as photo:
+                bot.send_photo(
+                    chat_id=os.environ['CHAT_ID'],
+                    photo=photo
+                )
+            time.sleep(86400)
+
+
 def main():
     images_folder = 'images'
     Path(images_folder).mkdir(parents=True, exist_ok=True)
@@ -14,16 +27,8 @@ def main():
     fetch_nasa_apod_images(images_folder)
     fetch_nasa_epic_images(images_folder)
     fetch_spacex_last_launch(images_folder)
-    bot = telegram.Bot(token=os.environ['BOT_TOKEN'])
-    photos = os.listdir('images')
-    while True:
-        for photo in photos:
-            with open('images/{}'.format(photo), 'rb') as photo:
-                bot.send_photo(
-                    chat_id=os.environ['CHAT_ID'],
-                    photo=photo
-                )
-            time.sleep(86400)
+    upload_images(images_folder)
+    
 
 
 if __name__ == '__main__':
